a6a783b4c1b51870cbb09046b68e25ed44a07bc5
MacMini34 <macmini34@MacMini34s-Mac-mini.local>
2019-01-31 19:59:19 +0530

    port changes

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ mongoose.connect(dbConfig.url, {
 
 require('./routes/router.js')(app);
 // listen for requests
-// app.listen(3000, () => {
-//     console.log("Server is listening on port 3000");
-// });
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
